7442e977399b3f0549a80555b8db8d1c1c723529
sanddrika <125376400+sanddrika@users.noreply.github.com>
2023-03-29 22:41:22 +0400

    done

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ let button = document.getElementById ('buttonClick');
 button.addEventListener ('click', function(){
     alert("page is in development, try again later")
 })
+let section1navigation = document.getElementById('section1mainnav');
+let burgerstart = document.getElementById('bar1');
+
+burgerstart.addEventListener('click',function(){
+    section1navigation.classList.toggle('new')
+})
---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById ('buttonClick');
+button.addEventListener ('click', function(){
+    alert("page is in development, try again later")
+})
+// let section1navbar = document.getElementById('newText');
+// let burgerbar = document.getElementById('burger');
+
+// burgerbar.addEventListener('click',function() {
+//     section1navbar.classList.toggle('newword')
+// })
+
+
